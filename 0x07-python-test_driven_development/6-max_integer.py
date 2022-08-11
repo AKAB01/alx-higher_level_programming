4d26b50ba46291c83dda53e8c6b97ec2679b047a
Abdurrahmon Akesire <101411673+AKAB01@users.noreply.github.com>
2022-08-11 15:10:39 +0100

    Max Integer

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,68 +1,18 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""Module to find the max integer in a list
 """
-import unittest
-max_integer = __import__('6-max_integer').max_integer
 
 
-class TestMaxInteger(unittest.TestCase):
-    """ Class to verify all edge cases in this function"""
-
-    def negatives(self):
-        """ Biggest between negatives number"""
-        a = [-5, -100, -2, -1000]
-        self.assertEqual(max_integer(a), -2)
-
-    def positives(self):
-        """ Biggest between positive number"""
-        a = [5, 100, 2, 1000]
-        self.assertEqual(max_integer(a), 1000)
-
-    def both_posneg(self):
-        """ Test mixing negatives and positives numbers"""
-        a = [-2, -1, 0, 1, 2]
-        self.assertEqual(max_integer(a), 2)
-
-    def one_float(self):
-        """ If one element is float"""
-        a = [1, 2, 3.3, 4]
-        self.assertEqual(max_integer(a), 4)
-
-    def all_float(self):
-        """ Teste when all elements are float"""
-        a = [1.1, 2.2, 3.3, 4.4]
-        self.assertEqual(max_integer(a), 4.4)
-
-    def all_same(self):
-        """ Test when all elements are the same number"""
-        a = [0, 0, 0, 0]
-        self.assertEqual(max_integer(a), 0)
-
-    def uniq_element(self):
-        """ Test when there is only 1 element"""
-        a = [0]
-        self.assertEqual(max_integer(a), 0)
-
-    def empty_list(self):
-        """ Test if we input empty lists"""
-        a = []
-        self.assertIsNone(max_integer(a))
-
-    def type_error(self):
-        """ When is typed a non-float-int number"""
-        a = [1, 2, "three", 4]
-        with self.assertRaises(TypeError):
-            max_integer(a)
-
-    def none_case(self):
-        """ None as argument"""
-        with self.assertRaises(TypeError):
-            max_integer(None)
-
-    def int_case(self):
-        """ int as argument"""
-        with self.assertRaises(TypeError):
-            max_integer(0)
-
-if __name__ == '__main__':
-    unittest.main()
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
